045fb4c901dd94bde0f8bcce18144062be2de540
Tomasz <tomasz.filipek89@gmail.com>
2026-04-01 21:01:47 +0200

    Finishing basic network module.

---
+++ modules/networking/outputs.tf
@@ -0,0 +1,31 @@
+# modules/networking/outputs.tf
+
+output "vnet_id" {
+  description = "ID of the virtual network"
+  value       = azurerm_virtual_network.virtual-network.id
+}
+
+output "vnet_name" {
+  description = "Name of the virtual network"
+  value       = azurerm_virtual_network.virtual-network.name
+}
+
+output "app_subnet_id" {
+  description = "ID of the application subnet"
+  value       = azurerm_subnet.app-subnet.id
+}
+
+output "db_subnet_id" {
+  description = "ID of the database subnet"
+  value       = azurerm_subnet.db-subnet.id
+}
+
+output "nsg_app_id" {
+  description = "ID of the app NSG"
+  value       = azurerm_network_security_group.nsg-app.id
+}
+
+output "nsg_db_id" {
+  description = "ID of the database NSG"
+  value       = azurerm_network_security_group.nsg-db.id
+}
