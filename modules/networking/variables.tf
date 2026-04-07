dea9ef5dba71f6b385b65cb1b428bab5f7d5f418
Tomasz <tomasz.filipek89@gmail.com>
2026-04-07 18:50:48 +0200

    testing CI/CD pipeline

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -21,7 +21,6 @@ variable "vnet_address_space" {
   type        = list(string)
   default     = ["10.0.0.0/16"]
 }
-
 variable "app_subnet_name" {
   description = "Name of the application subnet"
   type        = string
